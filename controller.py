9799a51fd03a9f5e08922bbbf28f930cd9d3c493
fatpav <fat_pav@hotmail.com>
2020-09-24 16:24:31 +0100

    trying to append the event list

--- controller.py
+++ controller.py
@@ -1,11 +1,21 @@
 from app import app
-from flask import render_template
-from app.models.event_list import events
+from flask import render_template, request, redirect
+from app.models.event_list import events, add_new_event
+from app.models.event import *
 
 @app.route('/')
 def index():
     return render_template('index.html', title = 'Home', events = events)
 
-# @app.route('/add-event', methods=['post'])
-# def add_event():
-#     return "Perty posted"
+@app.route('/add-event', methods=['post'])
+def add_event():     
+    event_name = request.form["name"]
+    event_date = request.form["date"]
+    event_guest_number = request.form["guest_number"]
+    event_location = request.form["location"]
+    event_description = request.form["description"]
+    new_event = Event(event_name, event_date, event_guest_number, event_location, event_description)
+    add_new_event(new_event)
+    return redirect('/')
+
+    
--- models/event.py
+++ models/event.py
@@ -1,8 +1,8 @@
 class Event():
 
-    def __init__(self, date, name, guest_number, location, description):
-        self.date = date
+    def __init__(self, name, date, guest_number, location, description):       
         self.name = name
+        self.date = date
         self.guest_number = guest_number
         self.location = location
         self.description = description
