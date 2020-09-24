398d27c6aaa1e13f5de8ac9159c59c3806c550f2
fatpav <fat_pav@hotmail.com>
2020-09-24 16:03:27 +0100

    Form built, button added

--- controller.py
+++ controller.py
@@ -5,3 +5,7 @@ from app.models.event_list import events
 @app.route('/')
 def index():
     return render_template('index.html', title = 'Home', events = events)
+
+# @app.route('/add-event', methods=['post'])
+# def add_event():
+#     return "Perty posted"
--- models/event_list.py
+++ models/event_list.py
@@ -3,3 +3,6 @@ from app.models.event import *
 event_1 = Event("2020-10-08","Pauls disco",6,"Burger King", "hot beats and a hotter ass")
 event_2 = Event("2020-11-09", "Cool Daze", 100, "Steve's House", "Autumnal vibes")
 events = [event_1, event_2]
+
+def add_new_event(event):
+    events.append(event)
